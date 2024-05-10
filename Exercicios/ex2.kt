583b31388999b67929a6100b4afd4d83dee58b79
Miguel Yudi <miguel.yudi.baba@gmail.com>
2024-05-09 23:22:17 -0300

    Fazendo commit dos exercicios do curso "More Kotlin Fundamentals" do Android Developer

---
+++ Exercicios/ex2.kt
@@ -0,0 +1,18 @@
+enum class Daypart {
+    morning,afternoon,evening
+}
+
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val duration: Int,
+)
+
+fun main() {
+    val evento = Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart=Daypart.morning, duration=15)
+    println("Title: "+evento.title)
+	println("Description: "+evento.description)
+    println("Daypart: "+evento.daypart)
+    println("Duration: "+evento.duration)
+}
